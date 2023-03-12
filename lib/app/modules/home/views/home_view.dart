09e5af81efb672e74ab1c1bf524cd4f9a761b460
raiszidane95 <raiszidane95@gmail.com>
2023-03-12 08:43:09 +0700

    feat : Add task card and add card widget

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../../core/utils/extension.dart';
 
+import '../../../core/utils/extension.dart';
+import '../../../core/value/colors.dart';
+import '../../widgets/add_card.dart';
+import '../../widgets/task_card.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -12,13 +15,42 @@ class HomeView extends GetView<HomeController> {
       body: SafeArea(
         child: ListView(
           children: [
-            Text(
-              'My List',
-              style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
+            Padding(
+              padding: EdgeInsets.all(4.0.wp),
+              child: Text(
+                'My List',
+                style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Obx(
+              () => GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                children: [
+                  ...controller.tasks
+                      .map((element) => LongPressDraggable(
+                          onDragStarted: () => controller.changeDeleting(true),
+                          onDraggableCanceled: (_, __) => controller.changeDeleting(false),
+                          onDragEnd: (_) => controller.changeDeleting(false),
+                          feedback: Opacity(
+                            opacity: 0.8,
+                            child: TaskCard(task: element),
+                          ),
+                          child: TaskCard(task: element)))
+                      .toList(),
+                  AddCard()
+                ],
+              ),
             ),
           ],
         ),
       ),
+      floatingActionButton: Obx(() => FloatingActionButton(
+            backgroundColor: controller.deleting.value ? Colors.red : blue,
+            onPressed: () {},
+            child: Icon(controller.deleting.value ? Icons.delete : Icons.add),
+          )),
     );
   }
 }
